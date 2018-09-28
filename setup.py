7e16d982f62421aabce9f663e010d7332f644e8b
Chris Warrick <kwpolska@gmail.com>
2018-09-28 23:03:53 +0200

    Forgot to bump setup.py version
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 from setuptools import setup, find_packages
 
 setup(name='django-expenses',
-      version='0.1.0',
+      version='0.1.1',
       description='A comprehensive system for managing expenses',
       keywords='django,expenses',
       author='Chris Warrick',
